144a0d11734c7baca1272ad2e0be97c217ff07ea
fansy1990 <fansy1990@foxmail.com>
2017-12-26 21:19:16 +0800

    Initial commit

---
+++ src/main/java/blog/BlogMvcApplication.java
@@ -0,0 +1,14 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fanzhe on 2017/12/26.
+ */
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BlogMvcApplication.class,args);
+    }
+}
